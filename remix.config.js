ed555a7fbccf66a9409f30ac0a57c856b5629c23
Kevin Green <kevin@ctrlaltdel.world>
2023-07-23 14:49:54 -0700

    change the remix config to node

--- remix.config.js
+++ remix.config.js
@@ -21,6 +21,6 @@ module.exports = {
   serverConditions: ['worker', process.env.NODE_ENV],
   serverDependenciesToBundle: 'all',
   serverModuleFormat: 'esm',
-  serverPlatform: 'neutral',
+  serverPlatform: 'node',
   serverMinify: process.env.NODE_ENV === 'production',
 };
